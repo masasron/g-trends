95b4bdf3652b181eb66da52b35060b930c126808
masasron <ronmasas@gmail.com>
2017-10-28 22:44:45 +0300

    progress

--- test/index.js
+++ test/index.js
@@ -1,24 +1,15 @@
 'use strict'
 
-const { ExploreTrendRequest,DownloadTrendRequest } = require('../')
+const { ExploreTrendRequest,SearchProviders } = require('../')
 
 const explore_request = new ExploreTrendRequest()
 
-explore_request.setEngine('web')
+explore_request.searchProvider(SearchProviders.Web)
                .past30Days()
-               .addKeyword('Dream about snakes')
-               .compare('Dream about falling')
-               .fetch().then( download_request_details => {
-        
-     const download_request = new DownloadTrendRequest(download_request_details)
-     
-     download_request.fetch().then( csv_data => {
-        console.log('[✔] Done, take a look at your beautiful CSV formatted data!')
-        console.log(csv_data)
-     }).catch( error => {
-         console.log('[!] Failed fetching csv data due to an error',error)
-     })
-
-}).catch( error => {
-    console.log('[!] Failed fetching widget request data due to an unexpected server response',error)
-})
+               .addKeyword('Dream about snakes').compare('Dream about falling')
+               .download().then( csv => {
+                    console.log('[✔] Done, take a look at your beautiful CSV formatted data!')
+                    console.log(csv)
+                }).catch( error => {
+                    console.log('[!] Failed fetching csv data due to an error',error)
+                })
